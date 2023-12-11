d0d6b7fef4ac6b6bee34800fc802933e94e8b825
Roquetteur2 <94811107+Roquetteur2@users.noreply.github.com>
2023-12-11 10:11:10 -0500

    Add files via upload

---
+++ Intro-to-Vue-3-L11-end/components/ProductDisplay.js
@@ -0,0 +1,88 @@
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <p>Shipping: {{ shipping }}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div 
+          v-for="(variant, index) in variants" 
+          :key="variant.id" 
+          @mouseover="updateVariant(index)" 
+          class="color-circle" 
+          :style="{ backgroundColor: variant.color }">
+        </div>
+        
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
+  </div>`,
+  data() {
+    return {
+        product: 'Gaming Mouse',
+        brand: 'Logitech',
+        selectedVariant: 0,
+        details: ['Light', 'Wireless', 'Gaming mouse'],
+        variants: [
+          { id: 3051, color: 'blue', image: './assets/images/g305-blue.png', quantity: 50 },
+          { id: 3052, color: 'turquoise', image: './assets/images/g305-turquoise.png', quantity: 0 },
+          { id: 3053, color: 'black', image: './assets/images/g305-black.png', quantity: 40 },
+        ],
+        reviews: []
+    }
+  },
+  methods: {
+      addToCart() {
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      },
+      addReview(review) {
+        this.reviews.push(review)
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
+        }
+        return 2.99
+      }
+  }
+})
